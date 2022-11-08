c418205ead9d81f7f0f6cefd4b9618fd3a55ba49
Yi-Wen LIN <bongiovi015@gmail.com>
2022-11-08 12:44:20 +0000

    build

--- wensday/src/components/landing/index.js
+++ wensday/src/components/landing/index.js
@@ -13,8 +13,10 @@ const Landing = () => {
         <div className="landing-title-container">
           <p>
             WENSDAY is the portfolio of Yi-Wen LIN — creative coder and
-            generative artist from Taiwan. Based in London, UK. Working remotely
-            with clients and partners worldwide.
+            generative artist from Taiwan. <br />
+            <br />
+            Based in London, UK. Working remotely with clients and partners
+            worldwide.
           </p>
         </div>
       </div>
