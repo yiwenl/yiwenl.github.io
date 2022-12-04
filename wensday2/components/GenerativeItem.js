7ecf7bd5f77c0b15415aa0d3ee41d688b96e21b0
Yi-Wen LIN <bongiovi015@gmail.com>
2022-12-04 12:58:19 +0000

    add generative page

---
+++ wensday2/components/GenerativeItem.js
@@ -0,0 +1,24 @@
+import styles from "../styles/Generative.module.css";
+import Image from "next/image";
+const GenerativeItem = ({ project, small }) => {
+  console.log(project);
+  const _isSmall = small === undefined ? false : small;
+  const style = _isSmall ? `${styles.item} ${styles.itemSmall}` : styles.item;
+
+  const path = `/assets/generative/${project.path}`;
+
+  return (
+    <div className={style}>
+      <div className={styles.imageWrapper}>
+        <a href={project.url} target="_blank">
+          <Image src={path} fill alt={project.title} />
+        </a>
+      </div>
+      <a href={project.url} target="_blank">
+        <h2 className={styles.projectTitle}>{project.title}</h2>
+      </a>
+    </div>
+  );
+};
+
+export default GenerativeItem;
--- wensday2/pages/generative.js
+++ wensday2/pages/generative.js
@@ -1,14 +1,50 @@
+// Styles
+import styles from "../styles/Generative.module.css";
+
+// Model
+import SiteData from "../model/data";
+
 import Meta from "../components/Meta";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import GenerativeItem from "../components/GenerativeItem";
 
-const Info = () => {
+const Generative = () => {
+  const { generatives } = SiteData;
+  console.log("generative", generatives);
   return (
     <>
       <Meta title={"Wensday | Generative"} />
       <Header />
-      <p>Generative</p>
+      <div className={styles.container}>
+        <h1 className={styles.title}>
+          Generative Art
+          <br />
+          Collections
+        </h1>
+        <div className={styles.wrapper}>
+          <div className={styles.column}>
+            <GenerativeItem project={generatives.fireWithin} />
+            <GenerativeItem small={true} project={generatives.strand} />
+            <GenerativeItem small={true} project={generatives.labyrinth} />
+            <GenerativeItem project={generatives.maze} />
+            <GenerativeItem project={generatives.threads} />
+          </div>
+          <div className={styles.column}>
+            <GenerativeItem small={true} project={generatives.collage} />
+            <GenerativeItem small={true} project={generatives.sketching} />
+            <GenerativeItem small={true} project={generatives.wondering} />
+            <GenerativeItem small={true} project={generatives.percentage} />
+            <GenerativeItem project={generatives.hex} />
+            <GenerativeItem small={true} project={generatives.rubixCube} />
+            <GenerativeItem small={true} project={generatives.excursion} />
+            <GenerativeItem project={generatives.circles} />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 };
 
-export default Info;
+export default Generative;
